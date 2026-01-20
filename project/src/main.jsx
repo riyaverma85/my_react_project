386d63726eba77b80f54db192bbc6bb06e05a3a9
riyaverma85 <riyaverma2976@gmail.com>
2026-01-19 18:19:42 -0800

    ,.,..,

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './Login'
+import App from './Router.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
  <App />
-</BrowserRouter>
+</BrowserRouter>    
 
    
  
