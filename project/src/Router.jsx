be5573f4094e86d4ddae4b6854d0d06784e4829e
riyaverma85 <riyaverma2976@gmail.com>
2026-02-02 22:48:36 -0800

    change

--- project/src/Router.jsx
+++ project/src/Router.jsx
@@ -21,7 +21,7 @@ const App = () => {
         <Route path="about" element={<About/>}/>
         <Route path="contact" element={<Contact/>}/>
         <Route path="service" element={<Service/>}/>
-       <Route path="movie/:type" element={<MovieDetails />} />
+         <Route path="movie/:type" element={<MovieDetails />} />
          <Route path='movies' element={<Movies/>}/>
         </Route>
     </Routes>
